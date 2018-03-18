739b3c5edceb3528c727e1332f5bf0abc40a6640
crisysy <cristinapavel.g.ro@gmail.com>
2018-03-18 20:09:10 +0200

    original commit

---
+++ Attendance-master/src/app/_models/user.ts
@@ -0,0 +1,12 @@
+export class User {
+    id: number;
+    username: string;
+    password: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    year: string;
+    group: string;
+    series: string;
+    studentID: string;
+}
